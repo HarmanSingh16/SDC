f8fc89f6e70f0345f74d65346dce7ad43e4183c6
HarmanSingh16 <singhharman160201@gmail.com>
2023-10-03 17:37:54 +0530

    Made changes to database_setup

--- src/sdc/database_setup.java
+++ src/sdc/database_setup.java
@@ -28,32 +28,17 @@ public class database_setup{
 // Resource function
 	// Creating table student if it does not exist
 		st.execute("create table IF NOT EXISTS studentDetails(\r\n"
-				+ "	uid	CHAR(8) Primary key,\r\n"
-				+ "    name VARCHAR(50) NOT NULL,\r\n"
-				+ "    class INT NOT NULL,\r\n"
-				+ "    section VARCHAR(1) NOT NULL,\r\n"
-	         + "    type INT NOT NULL,\r\n"
-				+ "    constraint branch_code_check check (uid like \"%KR%\" OR  uid like \"%IN%\" OR uid like \"%GM%\"),\r\n"
+				+ "	uid CHAR(8) Primary Key,\r\n"
+				+ "    name VARCHAR(50) not null,\r\n"
+				+ "    class INT not null,\r\n"
+				+ "    section char(1) not null,\r\n"
+				+ "    type int not null,\r\n"
+				+ "    constraint branch_code_check check(uid like \"%kr%\" or uid like \"%in%\" or uid like \"gm%\"),\r\n"
 				+ "    constraint primary_key_length check (LENGTH(uid) = 8),\r\n"
-				+ "    constraint name_length_check check (length(name) > 0)\r\n"
+				+ "    constraint name_length_check check (length(name) > 0),\r\n"
+				+ "    constraint class_check check(class > 0 AND class <13)\r\n"
 				+ ");");
 		 
     }
-	}
+}
 
-
-
-//Resource function
-	//Creating table student if it does not exist
-	// 	st.execute("create table IF NOT EXISTS studentDetails(\r\n"
-	// 			+ "	uid	CHAR(8) Primary key,\r\n"
-	// 			+ "    name VARCHAR(50) NOT NULL,\r\n"
-	// 			+ "    class INT NOT NULL,\r\n"
-	// 			+ "    section VARCHAR(1) NOT NULL,\r\n"
-	//          + "    type INT NOT NULL,\r\n"
-	// 			+ "    constraint branch_code_check check (uid like \"%KR%\" OR  uid like \"%IN%\" OR uid like \"%GM%\"),\r\n"
-	// 			+ "    constraint primary_key_length check (LENGTH(uid) = 8),\r\n"
-	// 			+ "    constraint name_length_check check (length(name) > 0)\r\n"
-	// 			+ ");");
-		 
-    // }
