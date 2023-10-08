236b26335e8fb5181d719cedbef7729d1dd4cd1e
HarmanSingh16 <singhharman160201@gmail.com>
2023-10-08 21:45:22 +0530

    SQL file added for initial database setup

--- SQL FUNCTION.sql
+++ SQL FUNCTION.sql
@@ -2,25 +2,36 @@ create database springdalecollege;
 use springdalecollege;
 
 select * from studentDetails;
-CREATE USER IF NOT EXISTS 'kr004028''@''%' IDENTIFIED BY 'hs@160201';
 
 create table IF NOT EXISTS teacherDetails(
 	uid CHAR(8) Primary key,
     name VARCHAR(50) NOT NULL,
-    classes JSON NOT NULL,
+   --  classes JSON NOT NULL,
     classTeacherOf VARCHAR(3) NOT NULL,
-    subjects JSON NOT NULL,
+    -- subjects JSON NOT NULL,
     type INT not null
 );
+drop table teacherDetails;
+insert into teacherDetails values("kr003438","Harman","XIA",2);
+select * from teacherDetails;
+
 create table IF NOT EXISTS studentDetails(
 	uid CHAR(8) Primary Key,
     name VARCHAR(50) not null,
     class INT not null,
     section char(1) not null,
+    dob date not null,
+    f_name Varchar(50) not null,
+    m_name varchar(50) not null,
+    phn_num varchar(10) not null,
     type int not null,
-    check (uid like "KR" OR "GM" OR "IN"),
-    check (class>=0 AND class<=12)
+    constraint uid_check check (uid like "%kr%" OR "%gm%" OR "%in%"),
+    constraint class_check check (class>=0 AND class<=12),
+    constraint phone_check check(length(phn_num) = 10),
+    constraint not_empty check(length(name) or length(f_name) or length(m_name) > 0)
 );
+select * from studentDetails;
+drop table studentDetails;
 
-
+insert into teacherDetails VALUES("KR004028","Harman","[11,12]","11A","['Hindi', 'English']");
 drop table teacherDetails;
